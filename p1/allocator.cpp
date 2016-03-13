39177da6efd9d13402b01be77666545292771656
Denis <rbbozk@yandex.ru>
2016-03-13 04:30:48 +0300

    Allocator 1.0

--- p1/allocator.cpp
+++ p1/allocator.cpp
@@ -1,3 +1,147 @@
 #include "allocator.h"
 
+Pointer::Pointer(unsigned int k, Allocator *_allocator){
+        allocator = _allocator;
+        number = k;
+    }
+void *Pointer::get() const { return ((allocator->ptrs)[number]).get(); } 
 
+Allocator::Ptr Allocator::_alloc(Header *h, size_t N){
+	if (h->size < N) {throw AllocErrorType::NoMemory;}
+	size_t last_size = h->size;
+	int tmp = last_size - (N + sizeof(Header));
+	if (tmp > 0){
+		Header *g = (Header *)((char *)h + N + sizeof(Header));
+		g->size = tmp;
+		h->size = N;
+		g->freed = true;
+	}
+	h->freed = false;
+	return Ptr(h);
+}
+
+void Allocator::sealing() {
+	char *cur = (char *)base;
+	Header *h2, *h1;
+	h1 = (Header *)cur;
+	cur += h1->size + sizeof(Header);
+	h2 = (Header *)cur; 
+	while (cur - (char *)base <= subsize){
+		if (h1->freed && h2->freed){
+			h1->size += (h2->size + sizeof(Header));
+		} else {
+			h1 = h2;
+		}
+		cur += h2->size + sizeof(Header);
+		h2 = (Header *)cur;
+	}
+}
+
+void Allocator::realloc(Pointer &p, size_t N){
+	Ptr *ptr;
+	auto it = ptrs.find(p.number);
+	if (it == ptrs.end()){
+		p = alloc(N);
+		return;
+	}
+/*	try {
+		ptr = &(ptrs.at(p.number));
+	} catch(std::out_of_range &){
+		throw AllocError(AllocErrorType::InvalidPtr, 
+			"try realloc nonexisting piece of memory");
+	} */
+	ptr = &it->second;
+	Header *h = ptr->h;
+	char *cur = (char *)h;
+	size_t oldsize = h->size;
+	cur += h->size + sizeof(Header);
+	Header *h2 = (Header *)cur;
+	if (h2->freed && h2->size + h->size + sizeof(Header) >= N){
+		size_t tmp = h2->size + h->size + sizeof(Header);
+		if (tmp - N > 2 * sizeof(Header)){
+			h->size = N;
+			cur = (char *)h + N + sizeof(Header);
+			h2 = (Header *)cur;
+			h2->freed = true;
+			h2->size = tmp - (N + sizeof(Header));
+			return;
+		} else
+		{
+			h->size += h2->size + sizeof(Header);
+			return;
+		}
+	}
+	Pointer tmpp = alloc(N);
+	std::memcpy(tmpp.get(), p.get(), oldsize);
+	Allocator::free(p);
+	p = tmpp;
+}
+
+void Allocator::free(Pointer &p) {
+	unsigned int n = p.number;
+	Ptr ptr;
+	try{
+	  ptr = ptrs.at(n);
+	} catch(std::out_of_range &t){
+		throw AllocError(AllocErrorType::InvalidFree, "free unallocated memory");
+	}
+	auto h = ptr.h;
+	h->freed = true;
+	ptrs.erase(n);
+	sealing();
+}
+
+void Allocator::defrag(){
+	void *old, *nevv;
+	if (activememory == ActiveMemory::First){
+		old = base1;
+		nevv = base = base2;
+		activememory = ActiveMemory::Second;
+	} else {
+		old = base2;
+		nevv = base = base1;
+		activememory = ActiveMemory::First;
+	}
+	char *cur = (char *)base;
+	for (auto i = ptrs.begin(); i != ptrs.end(); i++){
+			cur = (char *)rebase(&(i->second), (void *)cur);
+	}
+	Header *h = (Header *)cur;
+	h->freed = true;
+	h->size = subsize - (cur - (char*)base) - sizeof(Header);
+/*	Header *freeside = (Header *)old;
+	freeside->freed = true;
+	freeside->size = subsize - sizeof(Header); */
+}
+
+void *Allocator::rebase(Allocator::Ptr *ptr, void *newplace){
+	Header *h = ptr->h;
+	size_t size = h->size;
+	std::memcpy(newplace, h, size + sizeof(Header));
+	ptr->h = (Header *)newplace;
+	ptr->place = (void *)((char *) newplace + sizeof(Header));
+	return (void *)((char *)newplace + size + sizeof(Header));
+}
+
+Pointer Allocator::alloc(size_t N)
+{
+
+	char *cur = (char *)base;
+	Ptr p;
+	Header *h;
+	for (;;){
+		h = (Header *)cur;
+		if (h->freed && h->size >= N){
+			p = _alloc(h, N);
+			break;
+		}
+		cur += h->size + sizeof(Header);
+		if (cur - (char*)base > subsize){
+			throw AllocError(AllocErrorType::NoMemory,
+			  std::string("dont enough memory, try defrug"));
+		}
+	}
+	ptrs[nextint] = p;
+	return Pointer(nextint++, this);
+	
+}
--- p1/allocator.h
+++ p1/allocator.h
@@ -1,10 +1,17 @@
 #include <stdexcept>
-#include <string>
+#include <cstring>
+#include <list>
+#include <map>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
 
 enum class AllocErrorType {
     InvalidFree,
     NoMemory,
+    InvalidPtr
 };
+
 
 class AllocError: std::runtime_error {
 private:
@@ -22,19 +29,67 @@ public:
 class Allocator;
 
 class Pointer {
+    unsigned int number;
+    Allocator *allocator;
 public:
-    void *get() const { return 0; } 
+    Pointer(){number = 0; allocator = NULL;}
+    Pointer(unsigned int k, Allocator *_allocator);
+    void *get() const;
+    friend class Allocator;
 };
 
 class Allocator {
-public:
-    Allocator(void *base, size_t size) {}
-    
-    Pointer alloc(size_t N) { return Pointer(); }
-    void realloc(Pointer &p, size_t N) {}
-    void free(Pointer &p) {}
+    struct Header{
+        size_t size;
+        bool freed;
+    };
+    struct Ptr {
+        void *place;
+        Header *h;
+        Ptr(Header *_h){
+            h = _h;
+            place = (void *)((char *)h + sizeof(Header));
+        }
+        Ptr(){place = NULL; h=NULL;}
+        void *get() const { return place;}
+    };
+    enum class ActiveMemory{
+        First,
+        Second
+    } activememory;
 
-    void defrag() {}
+    void *base;
+    void *base1;
+    void *base2;
+    size_t allsize;
+    size_t subsize;
+    Ptr _alloc(Header *h, size_t N);
+    std::map<unsigned int, Ptr> ptrs;
+    unsigned int nextint;
+    void sealing();
+    void* rebase(Allocator::Ptr *, void *);
+    std::ofstream fout;
+public:
+    
+    Allocator(void *_base, size_t size) {
+        if (size < 2 * sizeof(Header)) { throw AllocErrorType::NoMemory; }
+        allsize = size;
+        subsize = size / 2;
+        activememory = ActiveMemory::First;
+        base = base1 = _base;
+        base2 = (void *)((char *)_base +subsize);
+        Header *h = (Header *)base;
+        h->size = subsize - sizeof(Header);
+        h->freed = true;
+        nextint = 1;
+    }
+    Pointer alloc(size_t N);
+    void realloc(Pointer &p, size_t N);
+    void free(Pointer &p);
+
+    void defrag();
     std::string dump() { return ""; }
+    friend class Pointer;
+    ~Allocator() {fout.close();}
 };
 
